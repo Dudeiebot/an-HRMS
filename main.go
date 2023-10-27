be120ee20aeeb55fa5156166dc9f915d590088e0
Dudeiebot <ojoexcel5@gmail.com>
2023-10-27 15:16:34 +0100

    more effieciency

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/gofiber/fiber/v2" //fiber take care of a lot of things for us from marshalling and unmarshalling our data
+	"github.com/gofiber/fiber/v2" // fiber take care of a lot of things for us from marshalling and unmarshalling our data
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -19,8 +19,10 @@ type MongoInstance struct {
 
 var mg MongoInstance
 
-const dbName = "fiber-hrms"
-const mongoURI = "mongodb://localhost:27017/" + dbName //if i use mlab it will give me a url that i can out here as mongodb and also it depends onn the way you configure your mongodb maybe with username and password or without
+const (
+	dbName   = "fiber-hrms"
+	mongoURI = "mongodb://localhost:27017/" + dbName // if i use mlab it will give me a url that i can out here as mongodb and also it depends onn the way you configure your mongodb maybe with username and password or without
+)
 
 type Employee struct {
 	ID     string  `json:"id,omitempty" bson:"_id,omitempty"` // first time using these id as a type in our struct
@@ -31,7 +33,10 @@ type Employee struct {
 
 func Connect() error {
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second) // these helps to check if your mongodb wont work
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		30*time.Second,
+	) // these helps to check if your mongodb wont work
 	defer cancel()
 
 	err = client.Connect(ctx)
@@ -49,25 +54,33 @@ func Connect() error {
 }
 
 func main() {
-
 	if err := Connect(); err != nil {
 		log.Fatal(err)
 	}
 	app := fiber.New()
 
 	app.Get("/employee", func(c *fiber.Ctx) error {
-
 		query := bson.D{{}}
+
+		count, err := mg.Db.Collection("employees").CountDocuments(c.Context(), query)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		// Create a slice with the calculated capacity based on the count
+		employees := make([]Employee, 0, count)
 
 		cursor, err := mg.Db.Collection("employees").Find(c.Context(), query)
 		if err != nil {
 			return c.Status(500).SendString(err.Error())
 		}
-		var employees []Employee = make([]Employee, 0)
 
-		if err := cursor.All(c.Context(), &employees); err != nil { //passing a reference to these var employees created
+		// Populate the slice with data from MongoDB
+		if err := cursor.All(c.Context(), &employees); err != nil {
 			return c.Status(500).SendString(err.Error())
 		}
+
+		// Return the employees slice
 		return c.JSON(employees)
 	})
 
@@ -81,13 +94,17 @@ func main() {
 		}
 		employee.ID = ""
 
-		insertionResult, err := collection.InsertOne(c.Context(), employee) //insertone is a func that i get mongodb and it takes data and insert it into the database, just one tho
-
+		insertionResult, err := collection.InsertOne(
+			c.Context(),
+			employee,
+		) // insertone is a func that i get mongodb and it takes data and insert it into the database, just one tho
 		if err != nil {
 			return c.Status(500).SendString(err.Error())
 		}
 
-		filter := bson.D{{Key: "_id", Value: insertionResult.InsertedID}} //just like how we created query in our app.GET route there
+		filter := bson.D{
+			{Key: "_id", Value: insertionResult.InsertedID},
+		} // just like how we created query in our app.GET route there
 		createdRecord := collection.FindOne(c.Context(), filter)
 
 		createdEmployee := &Employee{}
@@ -97,7 +114,6 @@ func main() {
 	})
 
 	app.Put("/employee/:id", func(c *fiber.Ctx) error {
-
 		idParam := c.Params("id")
 		employeeID, err := primitive.ObjectIDFromHex(idParam)
 		// these 2 line here is practically all the one line just as in ln 144, very easy and straightforward
@@ -112,7 +128,8 @@ func main() {
 
 		query := bson.D{{Key: "_id", Value: employeeID}}
 		update := bson.D{
-			{Key: "$set",
+			{
+				Key: "$set",
 				Value: bson.D{
 					{Key: "name", Value: employee.Name},
 					{Key: "age", Value: employee.Age},
@@ -132,20 +149,18 @@ func main() {
 
 		employee.ID = idParam
 		return c.Status(200).JSON(employee)
-
 	})
 
 	app.Delete("/employee/:id", func(c *fiber.Ctx) error {
-
 		employeeID, err := primitive.ObjectIDFromHex(c.Params("id"))
-
 		if err != nil {
-			return c.Status(400).SendString("Invalid Employer ID") //check out if the id i got here is valid and the error return the necessary important shii
+			return c.Status(400).
+				SendString("Invalid Employer ID")
+			// check out if the id i got here is valid and the error return the necessary important shii
 		}
 
 		query := bson.D{{Key: "_id", Value: employeeID}}
 		result, err := mg.Db.Collection("employees").DeleteOne(c.Context(), &query)
-
 		if err != nil {
 			return c.SendStatus(500)
 		}
@@ -155,7 +170,6 @@ func main() {
 		}
 
 		return c.Status(200).JSON("record deleted")
-
 	})
 
 	log.Fatal(app.Listen(":3000"))
